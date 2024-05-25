27f679b3cba15884294ed64d712fb39f5ec090b3
Ayazzzzzzz <149316954+Ayazzzzzzz@users.noreply.github.com>
2024-05-25 11:03:08 +0700

    Add files via upload

---
+++ project/src/project/Project.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project;
+
+/**
+ *
+ * @author Ayas
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login v = new login();
+        v.setVisible(true);
+        v.setLocationRelativeTo(null); 
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setLocationRelativeTo(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
